b7688630ec908922bbdacf5e533cba07a7d100ad
kouta-takagi <k-takagi@ruby-dev.jp>
2024-11-27 14:37:35 +0900

    current_userに変更

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,14 +2,14 @@ class TodosController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @finished_todos = User.find(4).todos.where(is_finished: true)
-    @unfinished_todos = User.find(4).todos.where(is_finished: false)
-    @user = User.find(4)
+    @finished_todos = current_user.todos.where(is_finished: true)
+    @unfinished_todos = current_user.todos.where(is_finished: false)
+    @user = current_user
     render json: {finished_todos:@finished_todos, unfinished_todos:@unfinished_todos, user: @user}
   end
 
   def create
-    @todo = User.find(params[:user_id]).todos.new(todo_params)
+    @todo = current_user.todos.new(todo_params)
     if @todo.save
       render status: :created
     else
